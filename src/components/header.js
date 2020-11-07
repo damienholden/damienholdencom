578c522e82e380bd1a7679f99243d7c8c9ffe710
dholden <dholden@cartrawler.com>
2020-11-07 23:29:11 +0000

    Formatted files

--- src/components/header.js
+++ src/components/header.js
@@ -2,26 +2,47 @@ import React from "react"
 
 import { Link } from "gatsby"
 
-import classNames from 'classNames'
+import classNames from "classNames"
 import headerStyles from "./header.module.scss"
 
-const Header = (props) => {
+const Header = props => {
   return (
     <header className={headerStyles.header}>
       <nav>
         <ul className={headerStyles.navList}>
           <li className={headerStyles.navListItemContainer}>
-            <Link className={classNames(props.theme === "dark" ? "dark" : "light", headerStyles.navItem)} activeClassName={headerStyles.activeNavItem} to="/">
+            <Link
+              className={classNames(
+                props.theme === "dark" ? "dark" : "light",
+                headerStyles.navItem
+              )}
+              activeClassName={headerStyles.activeNavItem}
+              to="/"
+            >
               Home
             </Link>
           </li>
           <li className={headerStyles.navListItemContainer}>
-            <Link className={classNames(props.theme === "dark" ? "dark" : "light", headerStyles.navItem)} activeClassName={headerStyles.activeNavItem} to="/projects">
+            <Link
+              className={classNames(
+                props.theme === "dark" ? "dark" : "light",
+                headerStyles.navItem
+              )}
+              activeClassName={headerStyles.activeNavItem}
+              to="/projects"
+            >
               Projects
             </Link>
           </li>
           <li className={headerStyles.navListItemContainer}>
-            <Link className={classNames(props.theme === "dark" ? "dark" : "light", headerStyles.navItem)} activeClassName={headerStyles.activeNavItem} to="/contact">
+            <Link
+              className={classNames(
+                props.theme === "dark" ? "dark" : "light",
+                headerStyles.navItem
+              )}
+              activeClassName={headerStyles.activeNavItem}
+              to="/contact"
+            >
               Contact
             </Link>
           </li>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,16 @@
 import React from "react"
 import Footer from "../components/footer"
 import Header from "../components/header"
-import classNames from 'classNames'
+import classNames from "classNames"
 import "../styles/index.scss"
 import layoutStyles from "./layout.module.scss"
 
 const Layout = props => {
-  const theme = !props.theme ? "dark" : props.theme;
+  const theme = !props.theme ? "dark" : props.theme
   return (
-    <section className={classNames('content-container', `${layoutStyles.container}`)}>
+    <section
+      className={classNames("content-container", `${layoutStyles.container}`)}
+    >
       <div className={layoutStyles.content}>
         <Header theme={theme} />
         {props.children}
