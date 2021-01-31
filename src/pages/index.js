1d1988457684811141615220143d1e22266168fe
dholden <dholden@cartrawler.com>
2021-01-31 17:04:41 +0000

    Cleaned up index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 import indexStyles from "./index.module.scss"
 
@@ -14,8 +14,7 @@ const IndexPage = () => {
         <h1>Hello,</h1>
         <h2>I'm Damien and I'm a software engineer based in Dublin Ireland.</h2>
         <p>
-          More info coming soon.
-          {/* Here are some of my <Link className={indexStyles.link}  to="/projects">projects</Link> */}
+          Here are some of my <Link className={indexStyles.link}  to="/projects">projects</Link>
         </p>
       </section>
     </Layout>
