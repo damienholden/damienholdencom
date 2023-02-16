6a14e35edfabf737a9d1ea299e472cfafb040c70
Damien Holden <damienholden@Damiens-MacBook-Pro.local>
2023-02-16 16:38:29 +0000

    Added React Helment to handle page titles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import theme from "../../gatsby-plugin-chakra-ui/theme";
 import Fonts from "../../gatsby-plugin-chakra-ui/fonts";
 import { useStaticQuery, graphql } from "gatsby";
-
+import { Helmet } from "react-helmet";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { Navigation } from "@components/Navigation";
 import { Footer } from "../Footer";
@@ -27,13 +27,11 @@ export const Layout = ({ pageName, children }) => {
       <Fonts />
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <ChakraProvider theme={theme}>
-        <main>
-          <title>
-            {pageName} | {author}
-          </title>
-          <Navigation />
-          {children}
-        </main>
+        <Helmet>
+          <title>{author ? `${pageName} | ${author}` : "Portfolio"}</title>
+        </Helmet>
+        <Navigation />
+        {children}
         <Footer />
       </ChakraProvider>
     </LayoutWrapper>
